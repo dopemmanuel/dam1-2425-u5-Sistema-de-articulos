b7fe16b387a85d487818725014caa7f3d87b2169
dopemmanuel <eolm728@g.educaand.es>
2025-02-21 16:18:54 +0100

    WIP...

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,3 @@
-package com.dam1.eju5.src
-
-/**
-Ejercicio 6: Sistema de Gestión de Vehículos
-Crea una jerarquía de clases para modelar un sistema de vehículos:
-
-Clase Vehículo con atributos como marca, modelo y año.
-Subclases Auto, Moto y Camion, con atributos adicionales como capacidadPasajeros,
-cilindrada, capacidadCarga, respectivamente.
-Añade métodos como acelerar(), frenar() y mostrarInformacion().
-
-Objetivos:
-Practicar la especialización de clases.
-Aplicar herencia y sobreescritura de métodos.
-Familiarizarse con objetos de diferentes categorías.
- * */
-
 fun main() {
-
+    println("Hello World!")
 }
